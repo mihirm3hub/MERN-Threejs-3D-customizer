2c6e072e065e2c032fcdb9dba22709c5919b8a7f
mihir <mihirmainkarm3@gmail.com>
2023-05-25 11:58:47 +0530

    fix server issues

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -54,7 +54,7 @@ const Customizer = () => {
       // call our backend to generate an ai image
       setGeneratingImg(true)
 
-      const response = await fetch('http://localhost:8080/api/v1/dalle', {
+      const response = await fetch('https://project-threejs-ai-4pf6.onrender.com/api/v1/dalle', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
